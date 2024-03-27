b73c28744d5a610d9bd65e29f08582ec1b81400a
Kota Mizushima <mizukota@gmail.com>
2024-03-27 13:29:27 +0900

    contains(@class, text) を使うように修正

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ with open('config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
 
 items = config['items']
-interval = config['interval']
 keywords = config['keywords']
 
 # ChromeDriverを起動
@@ -31,14 +30,15 @@ def save_screenshot(driver, label, directory_path, keywords):
     found = False
     visit_set = set()
     for keyword in keywords:
+        driver.refresh()
         keyword_elements = driver.find_elements(By.XPATH, f"//*[contains(text(), '{keyword}')]/../../..")
         for i, keyword_element in enumerate(keyword_elements):
             found = True
             if keyword_element in visit_set:
                 continue
             visit_set.add(keyword_element)
-            rank = keyword_element.find_element(By.XPATH, f".//div[@class='p-ranklist-item__place c-rank-place']/span")
-            author = keyword_element.find_element(By.XPATH, f".//div[@class='p-ranklist-item__author']/a")
+            rank = keyword_element.find_element(By.XPATH, f".//div[contains(@class, 'p-ranklist-item__place')]/span")
+            author = keyword_element.find_element(By.XPATH, f".//div[contains(@class, 'p-ranklist-item__author')]/a")
             print("ランキング: " + rank.text + "位")
             print("作者: " + author.text)
             element_screenshot = keyword_element.screenshot_as_png
@@ -51,7 +51,7 @@ def save_screenshot(driver, label, directory_path, keywords):
             now = datetime.datetime.now()
             now_text = now.strftime('%Y-%m-%d-%H%M')
 
-            path = f"{directory_path}/rank-{rank.text}_author-{author.text}_{now_text}.png"
+            path = f"{directory_path}/{label}_rank-{rank.text}_author-{author.text}_{now_text}.png"
             image.save(path)
             return path
 
@@ -64,10 +64,10 @@ def capture(label, url):
     # OSを判別して出力ディレクトリを設定
     if platform.system() == 'Windows':
         home_path = os.environ['HOMEPATH']
-        out_dir = f"{home_path}/Dropbox/gacha_contest/{label}"
+        out_dir = f"{home_path}/Dropbox/gacha_contest/"
     else:  # Linux の場合
         home_path = os.environ['HOME']
-        out_dir = f"{home_path}/Dropbox/gacha_contest/linux/{label}"
+        out_dir = f"{home_path}/Dropbox/gacha_contest/linux/"
     os.makedirs(out_dir, exist_ok=True)
 
     path = save_screenshot(
@@ -79,12 +79,8 @@ def capture(label, url):
     if path:
         print(f"スクリーンショット {path} をキャプチャしました")
 
-while True:
-    print("キャプチャ開始：")
-    for label, url in items.items():
-        capture(label + "_01_050", url + "?p=1")
-        capture(label + "_51_100", url + "?p=2")
-    print("キャプチャ終了：")
-    
-    # 指定した間隔だけ待機
-    time.sleep(interval)
+print("キャプチャ開始：")
+for label, url in items.items():
+    capture(label, url + "?p=1")
+    capture(label, url + "?p=2")
+print("キャプチャ終了：")
