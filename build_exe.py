87890382f6796aea82fe30ad6353d1afd68c8cf0
Kota Mizushima <mizukota@gmail.com>
2024-03-27 16:41:11 +0900

    実行可能ファイルを作れるように修正

--- build_exe.py
+++ build_exe.py
@@ -4,7 +4,6 @@ PyInstaller.__main__.run([
     'main.py',
     '--name=narou_capture',
     '--onefile',
-    '--noconsole',
-    '--add-data=config.json:.',
+    '--add-data=config.json;.',
     '--hidden-import=selenium.webdriver.chrome.options',
 ])
